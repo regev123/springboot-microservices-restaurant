2f60679e415b932ecdb71104246018938200629d
regev123 <regevregev8@gmail.com>
2025-09-13 07:56:54 +0300

    Initialize api_gateway_service

---
+++ api-gateway-service/src/test/java/com/restaurant/apiGateway/api_gateway_service/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.apiGateway.api_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
